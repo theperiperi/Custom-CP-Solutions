8f15236283837a33e26ebc7e84519836eb30a62a
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2023-12-31 12:59:28 +0530

    Add files via upload

---
+++ will it fit.py
@@ -0,0 +1,9 @@
+t=int(input())
+for i in range(t):
+    l=[int(j) for j in input().split()]
+    W=l[0]
+    L=l[1]
+    if W % 2 == 1 and L % 2 == 1:
+            print("No")
+    else:
+        print("Yes")
